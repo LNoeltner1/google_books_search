17c7786c7843cd137dc6afa6c891335508c15a5e
Lauren Noeltner <laurennoeltner@gmail.com>
2020-11-07 20:21:11 -0500

    fleshed out saved page and result component

---
+++ client/src/components/Result.jsx
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+import axios from "axios";
+
+const Result = (props) => {
+    const [savedBook, setSavedBook] = useState([]);
+    const [state, setState] = useState({
+        title: props.title,
+        authors: props.authors,
+        description: props.description,
+        image: props.image,
+        link: props.link,
+    });
+
+    const handleSave = (e) => {
+        axios.post("/api/books", state).then((res) => {
+            console.log(res);
+        });
+    };
+    const handleView = (e) => {
+        let path = state.link;
+        window.open(path);
+    };
+
+    return (
+        <div>
+            <div className="card">
+                <div className="row">
+                    <div className="col-md 3">
+                        <img alt={props.title} className="img-fluid" src={props.image} />
+                    </div>
+                    <div className="col-md 9">
+                        <div className="card-body">
+                            <h4 className="card-title">{props.title} by: {props.authors}</h4>
+                            <p>{props.description}</p>
+                        </div>
+                        <button onClick={handleSave} className="btn badge-pill btn-outline-warning mt-3 ml-3">Save</button>
+                        <button onclick={handleView} className="btn badge-pill btn-outline-warning mt-3 ml-3">View My Books</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- client/src/pages/Saved.jsx
+++ client/src/pages/Saved.jsx
@@ -1,13 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Card from "../components/Card";
 
-class Home extends Component {
-    render() {
-        return (
-            <div>Saved
-                
-            </div>
-        );
-    }
+const Saved = () => {
+    const[allBooks, setAllBooks] = useState ([]);
+
+    useEffect(() => {
+        renderBooks();
+    }, []);
+
+    const renderBooks = () => {
+        axios.get("/api/books").then((response) => {
+            setAllBooks(response.data);
+            console.log(response.data);
+        })
+    };
+
+    return (
+        <div className="container" style={{position: "relative"}}>
+            {allBooks.map((book) => (
+                <Card 
+                    title={book.title}
+                    authors={book.authors}
+                    description={book.description}
+                    link={book.link}
+                    image={book.image}
+                    id={book._id}
+                    renderBooks={renderBooks}
+                />
+            ))}
+        </div>
+    );
 }
 
-export default Home;
+
+export default Saved;
