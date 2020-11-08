c786baef84befbf6b49095fd75ce4f6cfab9a3d1
Lauren Noeltner <laurennoeltner@gmail.com>
2020-11-08 00:58:09 -0500

    removed commented out code in Books.js

--- models/Books.js
+++ models/Books.js
@@ -1,17 +1,3 @@
-// -- CREATE DATABASE google_booksDB;
-// -- USE google_booksDB;
-
-// -- CREATE TABLE 'books'
-// -- (
-// --     'id' int NOT NULL AUTO_INCREMENT,
-// --     'title' varchar,
-// --     'authors' varchar,
-// --     'description' varchar,
-// --     'image' varchar, 
-// --     'link' varchar,
-// --     PRIMARY KEY (id)
-// -- );
-
 const mongoose = require ("mongoose");
 const Schema = mongoose.Schema;
 
