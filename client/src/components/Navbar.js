add05f831ca123b8ea950a4fb3b48c59c077a776
Lauren Noeltner <laurennoeltner@gmail.com>
2020-11-08 00:05:57 -0500

    slightly modified navbar code; deployed to heroku again

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,13 +5,12 @@ class Navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-               
-                    <Link className="navbar-brand" to="/">Google Books</Link>
+               <div>
+                   <Link className="navbar-brand" to="/">Google Books</Link></div>
+                <div>
                     <Link className="nav-link" to="/search">Search</Link> 
                     <Link className="nav-link" to="/">Saved</Link> 
-                
-                
-
+                </div>
             </nav>
 
         );
